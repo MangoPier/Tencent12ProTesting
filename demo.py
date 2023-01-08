8a92ae4c31ef3de155fbc9716135486f2c621e08
MangoPier <didazhao@foxmail.com>
2023-01-08 15:30:23 +0800

    0108commit

---
+++ demo.py
@@ -0,0 +1,11 @@
+# from python.demo1 import Demo1
+from testing import demo2
+# from testing.demo2 import Demo2
+# from testing.demo2 import *
+from testing import *
+
+# Demo1()
+print(demo2.hello)
+demo2.f()
+
+# print(demo3.hello3)
